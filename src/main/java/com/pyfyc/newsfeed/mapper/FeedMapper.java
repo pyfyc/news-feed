e19f73ad4d6e46dc0d07d6ee4a410123ebfcd99b
Alex <alexeym75@gmail.com>
2023-09-01 12:31:14 +0700

    Removed commented out lines from FeedMapper

--- src/main/java/com/pyfyc/newsfeed/mapper/FeedMapper.java
+++ src/main/java/com/pyfyc/newsfeed/mapper/FeedMapper.java
@@ -11,10 +11,7 @@ import org.mapstruct.*;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = CategoryMapper.class)
 public interface FeedMapper {
 
-    //@Mapping(source = "name", target = "name")
-    //@Mapping(source = "description", target = "description")
     @Mapping(target = "publishDate", expression = "java(java.time.LocalDate.now())")
-    //@Mapping(source = "category", target = "category")
     void create(@MappingTarget Feed target, CreateFeedDto source);
 
     @Mapping(source = "name", target = "name", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
